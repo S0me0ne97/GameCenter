f020e19718e235541fbfd0df34f04ad75b4da0f5
Bálint Vida <e93r1v@inf.elte.hu>
2022-04-25 22:22:25 +0200

    Added content to contact.php

--- src/contact.php
+++ src/contact.php
@@ -4,6 +4,40 @@
 
     session_start();
     $auth = new Auth(new UserStorage());
+
+    $data = [];
+    $errors = [];
+
+    
+    if (count($_POST) > 0) {
+        $errors['name'] = '';
+        $errors['email'] = '';
+        $errors['text'] = '';
+        $errors['subject'] = '';
+        if (validate($_POST, $errors)) {
+            $authenticated_user = $auth->authenticated_user();
+            $data['name'] = $_POST['name'];
+            $data['email'] = $_POST['email'];
+            $data['text'] = $_POST['text'];
+            $data['subject'] = $_POST['subject'];
+            $data['date'] = date("Y-m-d");
+            #$contactStorage->add($data);
+        }
+    }
+    
+    function validate($post, &$errors) {
+        $emptyerror = "Nem lehet üresen hagyni!";
+        if (!isset($post['name']) || $post['name'] === '') {
+            $errors['name'] = $emptyerror;
+        }
+        if (!isset($post['email']) || $post['email'] === '') {
+            $errors['email'] = $emptyerror;
+        }
+        if (!isset($post['text']) || $post['text'] === '') {
+            $errors['text'] = $emptyerror;
+        }
+        return $errors['name'] === '' && $errors['email'] === '' && $errors['text'] === '';
+      }
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -29,6 +63,40 @@
             <?php endif ?>
         </ul> 
     </nav>
+
+    <h1>Lépjen velünk kapcsolatba!</h1>
+
+    <form action="" method="post" novalidate id="from">
+        <div>
+
+        </div>
+        <label for="name">Név:</label>
+        <input type="text" name="name" id="name" value="<?= $_POST['name'] ?? "" ?>">
+        <?php if(count($_POST) > 0 && $errors['name'] !== '') : ?>
+            <span class="error">
+                <?= $errors['name'] ?>
+            </span>
+        <?php endif; ?><br>
+        <label for="email">E-mail cím:</label>
+        <input type="text" name="email" id="email" value="<?= $_POST['email'] ?? "" ?>">
+        <?php if (isset($errors['email'])) : ?>
+            <span class="error"><?= $errors['email'] ?></span>
+        <?php endif; ?><br>
+        
+        <label for="subject">Levél téma:</label> <br>
+            <input type="radio" id="complain" name="subject" value="complain" checked>
+            <label for="complain">Panasz</label><br>
+            <input type="radio" id="feedback" name="subject" value="feedback">
+            <label for="feedback">Visszajelzés</label><br>
+        <label for="text">Szöveg:</label> <br>
+        <textarea name="text" id="text" cols="30" rows="10" ><?= $_POST['text'] ?? "" ?></textarea>
+        <?php if (isset($errors['text'])) : ?>
+            <span class="error"><?= $errors['text'] ?></span>
+        <?php endif; ?><br>
+        
+        <button type="submit">Elküld</button>
+    </form>
+
     <footer>
         <p>  
             Készítette: Vida Bálint - E93R1V
