27f79259a99468f439114c8fd15c84877f0a9b63
Bálint Vida <e93r1v@inf.elte.hu>
2022-05-13 13:39:22 +0200

    Added ship control

--- src/js/space_shooter.js
+++ src/js/space_shooter.js
@@ -1,4 +1,4 @@
-//variables
+//#region Variables
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var canvas_height = canvas.height;
@@ -9,14 +9,33 @@ var ship =
     x: (canvas_width - 20) / 2,
     y: canvas_height - 20,
     width: 20,
-    height: 20
+    height: 20,
+    speed: 400,
+    dir: 0,
 };
 
-//function definitions
-function gameLoop() {
+var lastTime = performance.now();
+
+var pressedKey = '';
+
+//#endregion Variables
+
+//#region Function definitions
+function gameLoop(now = performance.now()) {
+    const dt = (now - lastTime) / 1000;
+    lastTime = now;
+    
+    update(dt);
     draw();
+
     window.requestAnimationFrame(gameLoop);
 }
+
+function update(dt) {
+    ship.x += ship.dir * ship.speed * dt
+    
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
@@ -24,7 +43,30 @@ function draw() {
     ctx.fillRect(ship.x, ship.y, ship.width, ship.height);
 }
 
-//Event listener
+function onKeyDown(event) {
+    if (event.key === 'ArrowLeft') {
+        ship.dir = -1;
+        pressedKey = 'ArrowLeft';
+    }
+    if (event.key === 'ArrowRight') {
+        ship.dir = 1;
+        pressedKey = 'ArrowRight';
+    }
+}
 
-//Game loop
+function onKeyUp(event) {
+    if (event.key === pressedKey) {
+        ship.dir = 0;
+        pressedKey = '';
+    }
+}
+//#endregion Function definitions
+
+//#region Event listener
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+//#endregion Event listener
+
+//#region Game loop
 gameLoop();
+//#endregion Game loop
